363498e9be3f1c8a91ca7a53922f1a64ddc848e5
Sivaraman Raja <sivaramanr38@gmail.com>
2026-02-23 21:33:50 +0530

    Added Customer management service for cloud cuisine

---
+++ customer-service/src/test/java/com/cloudcuisine/customer_service/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudcuisine.customer_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
