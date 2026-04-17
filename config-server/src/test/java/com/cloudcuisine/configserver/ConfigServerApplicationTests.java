9923220ad551a5790cf6100e35f1c71d499890ba
Sivaraman Raja <sivaramanr38@gmail.com>
2026-04-17 10:44:57 +0530

    Initial commit for cloudcuisine-config-server

---
+++ config-server/src/test/java/com/cloudcuisine/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudcuisine.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
