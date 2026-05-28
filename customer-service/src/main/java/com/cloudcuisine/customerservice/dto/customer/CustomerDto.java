2a07860f3e0660eab10a2d091cb54a463e666fd2
sivaramanr38 <sivaramanr38@gmail.com>
2026-05-28 22:23:51 +0530

    Introduction to Jakarta validation for CustomerDto

--- customer-service/src/main/java/com/cloudcuisine/customerservice/dto/customer/CustomerDto.java
+++ customer-service/src/main/java/com/cloudcuisine/customerservice/dto/customer/CustomerDto.java
@@ -1,5 +1,8 @@
 package com.cloudcuisine.customerservice.dto.customer;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,11 +18,19 @@ import java.util.List;
 public class CustomerDto {
 
     private Long id;
+    @NotBlank(message = "First name can not be blank")
+    @Size(min = 2, max = 50, message = "First Name must be between 2 and 50 characters")
     private String firstName;
+
+    @NotBlank(message = "Last name can not be blank")
+    @Size(min = 1, max = 50, message = "Last name must be between 2 and 50 characters")
     private String lastName;
+
+    @Email(message = "Email should be valid")
     private String email;
 
     private List<PhoneDto> phones;
+
     private List<AddressDto> addresses;
 
     private boolean active;
